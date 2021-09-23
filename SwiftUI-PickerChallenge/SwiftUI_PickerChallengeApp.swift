2198cccd06e4d8b0e986f90db3744e382fa7946b
Tony-Kara <eniolaanthony@gmail.com>
2021-09-23 19:45:39 +0600

    Initial Commit

---
+++ SwiftUI-PickerChallenge/SwiftUI_PickerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PickerChallengeApp.swift
+//  SwiftUI-PickerChallenge
+//
+//  Created by mac on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PickerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
